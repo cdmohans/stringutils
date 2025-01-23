cb94f084941c3448d1efbab036b18574305ce7a0
Mohanasundaram Duraisamy <mohanasundaram.duraisamy@autodesk.com>
2025-01-23 10:51:19 +0800

    testing

---
+++ stringutils.go
@@ -0,0 +1,15 @@
+package stringutils
+
+import (
+	"strings"
+)
+
+// ToUpper converts a string to uppercase
+func ToUpper(s string) string {
+	return strings.ToUpper(s)
+}
+
+// ToLower converts a string to lowercase
+func ToLower(s string) string {
+	return strings.ToLower(s)
+}
